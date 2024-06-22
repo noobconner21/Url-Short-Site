dfbd14251489f16b45bf388e0cca44dd2efb3749
noobconner21 <kenwayshay21@gmail.com>
2024-06-22 15:32:50 +0530

    change layout

--- script.js
+++ script.js
@@ -3,19 +3,34 @@ const reloadBtn = document.getElementById('reload-btn');
 
 shortBtn.addEventListener('click', shortenUrl);
 
+function showLoading() {
+    document.getElementById("loading-screen").style.display = "flex";
+}
+
+function hideLoading() {
+    document.getElementById("loading-screen").style.display = "none";
+}
+
 function shortenUrl() {
     var originalUrl = document.getElementById("originalUrl").value;
     var apiUrl = "https://tinyurl.com/api-create.php?url=" + encodeURIComponent(originalUrl);
-    shortenedUrlTextarea = document.getElementById("shortenedUrl");
+    var shortenedUrlTextarea = document.getElementById("shortenedUrl");
+
+    showLoading();
 
     fetch(apiUrl).then(response => response.text()).then(data => {
         shortenedUrlTextarea.value = data;
+        hideLoading();
     }).catch(error => {
         shortenedUrlTextarea.value = "Error : Unable to shorten URL!";
+        hideLoading();
     });
-
 }
 
 reloadBtn.addEventListener('click', () => {
     location.reload();
+});
+
+window.addEventListener('load', () => {
+    hideLoading();
 });
